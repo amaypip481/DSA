35304e01f9ef61e63ebc7a7c5be4e104cda7e976
amaypip481 <179745277+amaypip481@users.noreply.github.com>
2025-02-02 19:11:37 +0530

    Recurrsive Quick Sort - optimized

--- quickSort.js
+++ quickSort.js
@@ -1,45 +1,45 @@
 function pivotSwap(arr){
     let pivot = arr[Math.round(arr.length/2)];
+    let pivotIndex = Math.round(arr.length/2);
     let i = 0;
     let j = 0;
-    while(i<arr.length && j<arr.length)
+    let pivotCounter = 0;
+   for (i=0; i<pivotIndex;)
+   {
+    if(arr[i]>pivot)
     {
-        
-        if(arr[j]>pivot && arr.indexOf(pivot)>j) 
-        {
-            arr.push(arr[j]);
-            arr.splice(j,1);         
-        }
-        else if(arr[j]>pivot && arr.indexOf(pivot)<j) 
-            {
-                j++;        
-            }
-        else if(arr[j]<pivot)
-        {
-            arr.unshift(arr[j]);
-            arr.splice(j+1,1);
-            i++;
-            j++;
-        }
-        else j++;
+        arr.push(arr[i]);
+        arr.splice(i,1);
+        pivotIndex--;
     }
-
+    else i++;
+   }
+   for (j=pivotIndex; j<arr.length; )
+    {
+     if(arr[j]<pivot)
+     {
+         arr.unshift(arr[j]);
+         arr.splice(j+1,1);
+         pivotIndex++;
+     }
+     else j++;
+    }
     // arr.splice(Math.round(arr.length/2),1);
     // arr.splice(i,0,pivot);
   
 
-    return {arr, i};
+    return {arr, pivotIndex };
 }
 
 
-let arr = [8, 3, 5, 2, 7, 6, 4, 1, 9];
+let arr = Array.from({ length: 100 }, () => Math.floor(Math.random() * 100));
 
 function quickSort(arr){
     if(arr.length <= 1) return arr;
 
     let pivotedArray = pivotSwap(arr);
 
-    return quickSort(pivotedArray.arr.slice(0, pivotedArray.i)).concat(quickSort(pivotedArray.arr.slice(pivotedArray.i,pivotedArray.length)));
+    return quickSort(pivotedArray.arr.slice(0, pivotedArray.pivotIndex)).concat(quickSort(pivotedArray.arr.slice(pivotedArray.pivotIndex,pivotedArray.length)));
     
 
 }
