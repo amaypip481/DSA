991baf9d9b3a3504cab21483f37ef7297fdeb77c
amaypip481 <179745277+amaypip481@users.noreply.github.com>
2025-02-02 16:32:31 +0530

    Recurrsive Quick Sort Solved

--- quickSort.js
+++ quickSort.js
@@ -1,39 +1,47 @@
 function pivotSwap(arr){
     let pivot = arr[Math.round(arr.length/2)];
     let i = 0;
-    let j = i+1;
+    let j = 0;
     while(i<arr.length && j<arr.length)
     {
-        if(arr[j]<pivot) 
+        
+        if(arr[j]>pivot && arr.indexOf(pivot)>j) 
         {
-            let temp = arr[j];
-            arr[j] = arr[i];
-            arr[i] = temp;
+            arr.push(arr[j]);
+            arr.splice(j,1);         
+        }
+        else if(arr[j]>pivot && arr.indexOf(pivot)<j) 
+            {
+                j++;        
+            }
+        else if(arr[j]<pivot)
+        {
+            arr.unshift(arr[j]);
+            arr.splice(j+1,1);
             i++;
-            j++;            
+            j++;
         }
         else j++;
     }
 
-    arr.splice(Math.round(arr.length/2),1);
-    arr.splice(i,0,pivot);
+    // arr.splice(Math.round(arr.length/2),1);
+    // arr.splice(i,0,pivot);
   
 
-    return arr;
+    return {arr, i};
 }
 
 
 let arr = [8, 3, 5, 2, 7, 6, 4, 1, 9];
 
 function quickSort(arr){
-    if(arr.length === 1) return arr;
+    if(arr.length <= 1) return arr;
 
-    let x = Math.round(arr.length/2);
-    let y = Math.round(arr.length);
+    let pivotedArray = pivotSwap(arr);
 
-    quickSort(arr.slice(0, x));
-    quickSort(arr.slice(x,y));
-    return pivotSwap(arr);
+    return quickSort(pivotedArray.arr.slice(0, pivotedArray.i)).concat(quickSort(pivotedArray.arr.slice(pivotedArray.i,pivotedArray.length)));
+    
+
 }
 
 console.log(quickSort(arr));
